b2c5c4bf8b57bfa8a647ce695f999623225df783
karthikr78677 <karthikr78677@gmail.com>
2025-06-06 10:30:22 +0530

    Fix: add react-scripts to dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from "./App.jsx"
 import store from "./store/store.jsx"
 import { Provider } from 'react-redux';
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App/>
